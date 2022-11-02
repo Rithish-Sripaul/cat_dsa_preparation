8d065293aee084b7423985e1f51c60d2b789f69d
Rithish-Sripaul <rithishsripaul@gmail.com>
2022-11-02 19:38:37 +0530

    Implementation of Stack with Push, Pop, Peek and Display

--- src/stackLinkedList/main.java
+++ src/stackLinkedList/main.java
@@ -1,4 +1,70 @@
 package stackLinkedList;
 
+class Stack {
+    Node head;
+
+    void push(int data) {
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = newNode;
+    }
+
+    void pop() {
+        Node temp = head;
+        while (temp.next.next != null) {
+            temp = temp.next;
+        }
+
+        temp.next = null;
+    }
+
+    void peek() {
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        System.out.println(temp.data);
+    }
+
+    void display() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+}
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
 public class main {
+    public static void main(String[] args) {
+        Stack st = new Stack();
+        st.push(1);
+        st.push(2);
+        st.push(3);
+        st.push(4);
+        st.display();
+        st.peek();
+        st.pop();
+        st.display();
+        st.peek();
+    }
 }
